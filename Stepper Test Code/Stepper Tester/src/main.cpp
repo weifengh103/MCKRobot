e06ccdef2daa32a4bda62091e44d29ee28f04244
weifengh103 <weifengh103@gmail.com>
2025-12-26 22:37:00 -0800

    First motor is sort of woking. It is slow.

--- Stepper Test Code/Stepper Tester/src/main.cpp
+++ Stepper Test Code/Stepper Tester/src/main.cpp
@@ -16,24 +16,27 @@ double thetas[] = {0, 0, M_PI/2, -M_PI, M_PI/2, 0};
 KinematicSolver robot(a, d, alphas, thetas);
 
 /* ---------------- Stepper wiring ---------------- */
-const int STEP_PIN[NUM_AXES] = {12, 14, 15, 16, 17, 21};
-const int DIR_PIN [NUM_AXES] = {13,22,23,24,25,26};
+const int STEP_PIN[NUM_AXES] = {13,14,26,33,16,18};
+const int DIR_PIN [NUM_AXES] = {12,27,25,32,17,19};
 
 /* ---------------- Stepper mechanics ---------------- */
-const int   STEPS_PER_REV = 400;
+const int   STEPS_PER_REV = 25000;
 const int   MICROSTEP     = 1;
 const float GEAR_RATIO[NUM_AXES] = {1,1,1,1,1,1};
 
-float STEPS_PER_RAD[NUM_AXES];
+float STEPS_PER_JointDeg[NUM_AXES];
 
 /* ---------------- Control timing ---------------- */
-const float Fs = 500.0f;
+// const float Fs = 500.0f;
+const float Fs =10.0f;
 const float Ts = 1.0f / Fs;
 const uint32_t Ts_us = (uint32_t)(1e6f * Ts);
 
+
+
 /* ---------------- Cartesian limits ---------------- */
-const float V_MAX = 0.10f;   // m/s
-const float A_MAX = 0.30f;   // m/s^2
+const float V_MAX = 1.0f;   // m/s
+const float A_MAX = 3.0f;   // m/s^2
 
 /* ============================================================
    HELPERS
@@ -62,8 +65,9 @@ long  prevStepTarget[NUM_AXES] = {0};
 
 inline void pulse(int pin) {
   digitalWrite(pin, HIGH);
-  delayMicroseconds(2);
+  delayMicroseconds(5);
   digitalWrite(pin, LOW);
+  
 }
 
 void emitSteps(float &acc, int stepPin, int dirPin) {
@@ -123,8 +127,8 @@ void setup() {
     pinMode(STEP_PIN[i], OUTPUT);
     pinMode(DIR_PIN[i], OUTPUT);
     digitalWrite(STEP_PIN[i], LOW);
-    STEPS_PER_RAD[i] =
-      (STEPS_PER_REV * MICROSTEP * GEAR_RATIO[i]) / (2.0f * PI);
+    STEPS_PER_JointDeg[i] =
+      (STEPS_PER_REV/360) * MICROSTEP * GEAR_RATIO[i];
   }
 
   Serial.println("6-DOF Cartesian DDA planner w/ rotation");
@@ -140,18 +144,18 @@ void loop() {
 
   /*Stepper test*/
 
-  // stepAcc[0] = 800.0f; // 2 revolutions
+  // stepAcc[0] = 25000.0f; // 2 revolutions
   // emitSteps(stepAcc[0], STEP_PIN[0], DIR_PIN[0]);
   
   
-   digitalWrite(DIR_PIN[0], HIGH); // Set direction
-  digitalWrite(STEP_PIN[0], HIGH); // Pulse HIGH (e.g., 1 microsecond)
-  delayMicroseconds(5);
-  digitalWrite(STEP_PIN[0], LOW);  // Pulse LOW (e.g., 1 microsecond)
-  delayMicroseconds(5);
-  // delayMicroseconds(1000); 
+  // //  digitalWrite(DIR_PIN[0], HIGH); // Set direction
+  // // digitalWrite(STEP_PIN[0], HIGH); // Pulse HIGH (e.g., 1 microsecond)
+  // // delayMicroseconds(20);
+  // // digitalWrite(STEP_PIN[0], LOW);  // Pulse LOW (e.g., 1 microsecond)
+  // // delayMicroseconds(20);
+  //  delayMicroseconds(1000); 
   
-  // Serial.println("Move complete");
+  //  Serial.println("Move complete");
 
 
 
@@ -163,83 +167,95 @@ void loop() {
   
 
 
-  // /* -------- Cartesian endpoints -------- */
-  // double P0[] = {0.30, 0.00, 0.30, 180, 0, 0};
-  // double P1[] = {0.40, 0.20, 0.25, 180, 0, 90};
+  /* -------- Cartesian endpoints -------- */
+  double P0[] = {50, 0, 50, 90, 0, 0};
+  double P1[] = {50, 20, 50, 90, 0, 0};
 
-  // /* ---- Orientation in radians ---- */
-  // double R0[3] = {
-  //   P0[3], P0[4], P0[5]
-  // };
-  // double R1[3] = {
-  //   P1[3], P1[4], P1[5]
-  // };
+  /* ---- Orientation in radians ---- */
+  double R0[3] = {
+    P0[3], P0[4], P0[5]
+  };
+  double R1[3] = {
+    P1[3], P1[4], P1[5]
+  };
 
-  // /* ---- Path direction ---- */
-  // float dx = P1[0] - P0[0];
-  // float dy = P1[1] - P0[1];
-  // float dz = P1[2] - P0[2];
-  // float D  = sqrt(dx*dx + dy*dy + dz*dz);
+  /* ---- Path direction ---- */
+  float dx = P1[0] - P0[0];
+  float dy = P1[1] - P0[1];
+  float dz = P1[2] - P0[2];
+  float D  = sqrt(dx*dx + dy*dy + dz*dz);
 
-  // /* ---- Unit vector along path ---- */
-  // float ux = dx / D;
-  // float uy = dy / D;
-  // float uz = dz / D;
+  /* ---- Unit vector along path ---- */
+  float ux = dx / D;
+  float uy = dy / D;
+  float uz = dz / D;
 
-  // Trapezoid tr = computeTrapezoid(D, V_MAX, A_MAX);
+  Trapezoid tr = computeTrapezoid(D, V_MAX, A_MAX);
 
-  // /* ---- Main control loop ---- */
-  // double q[NUM_AXES], prevQ[NUM_AXES];
+  /* ---- Main control loop ---- */
+  double q[NUM_AXES], prevQ[NUM_AXES];
 
-  // robot.solveIK(P0, true, prevQ);
-  // for (int i = 0; i < NUM_AXES; i++)
-  //   prevStepTarget[i] = lround(prevQ[i] * STEPS_PER_RAD[i]);
+  robot.solveIK(P0, true, prevQ);
+  for (int i = 0; i < NUM_AXES; i++)
+    prevStepTarget[i] = lround(prevQ[i] * STEPS_PER_JointDeg[i]);
 
-  // uint32_t tStart = micros();
-  // uint32_t nextSample = tStart;
+  uint32_t tStart = micros();
+  uint32_t nextSample = tStart;
 
-  // while (true) {
-  //   if (micros() < nextSample) continue;
-  //   nextSample += Ts_us;
+  while (true) {
+    if (micros() < nextSample) continue;
+    nextSample += Ts_us;
 
-  //   float t = (micros() - tStart) * 1e-6f;
+    float t = (micros() - tStart) * 1e-6f;
 
 
-  //   if (t > tr.T) 
-  //     t = tr.T;
+    if (t > tr.T) 
+      t = tr.T;
 
-  //   float s = currDist_of_t(tr, t);
-  //   double alpha = s / D;
+    float s = currDist_of_t(tr, t);
+    double alpha = s / D;
 
-  //   /* ---- Interpolated 6D pose ---- */
-  //   double P[6];
-  //   P[0] = P0[0] + ux * s;
-  //   P[1] = P0[1] + uy * s;
-  //   P[2] = P0[2] + uz * s;
+    /* ---- Interpolated 6D pose ---- */
+    double P[6];
+    P[0] = P0[0] + ux * s;
+    P[1] = P0[1] + uy * s;
+    P[2] = P0[2] + uz * s;
 
-  //   P[3] = R0[0] + alpha * (R1[0] - R0[0]);
-  //   P[4] = R0[1] + alpha * (R1[1] - R0[1]);
-  //   P[5] = R0[2] + alpha * (R1[2] - R0[2]);
+    P[3] = R0[0] + alpha * (R1[0] - R0[0]);
+    P[4] = R0[1] + alpha * (R1[1] - R0[1]);
+    P[5] = R0[2] + alpha * (R1[2] - R0[2]);
 
-  //   if (!robot.solveIK(P, true, q)) break;
+    if (!robot.solveIK(P, true, q)) break;
+    
+    // Serial.println( q[0]);
+    // q[0] = q[0] + 0.1;
 
-  //   for (int i = 0; i < NUM_AXES; i++) {
-  //     long targetSteps = lround(q[i] * STEPS_PER_RAD[i]);
-  //     stepAcc[i] += targetSteps - prevStepTarget[i];
-  //     prevStepTarget[i] = targetSteps;
-  //   }
+    for (int i = 0; i < NUM_AXES; i++) {
+      long targetSteps = lround(q[i] * STEPS_PER_JointDeg[i]);
+      stepAcc[i] += targetSteps - prevStepTarget[i];
+      prevStepTarget[i] = targetSteps;
+    }
+    
+    uint32_t emitStart = micros();
+    while (micros() - emitStart < Ts_us) {
+      // for (int i = 0; i < NUM_AXES; i++)
+      // {
+      //   emitSteps(stepAcc[i], STEP_PIN[i], DIR_PIN[i]);
+        
+      // }
+        for (int i = 0; i < 1; i++)
+      {
+        emitSteps(stepAcc[i], STEP_PIN[i], DIR_PIN[i]);
+        
+      }
+    }
 
-  //   uint32_t emitStart = micros();
-  //   while (micros() - emitStart < Ts_us) {
-  //     for (int i = 0; i < NUM_AXES; i++)
-  //       emitSteps(stepAcc[i], STEP_PIN[i], DIR_PIN[i]);
-  //   }
-
-  //   if (t >= tr.T) 
-  //     { Serial.println("Move complete");}
-  //   continue;
-  // }
+    if (t >= tr.T) 
+      { Serial.println("Move complete");}
+    continue;
+  }
 
  
-  // while (1) delay(1000);
+  while (1) delay(1000);
+  
 }
